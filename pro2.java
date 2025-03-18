f8ab55ba6c911000d5b982b072a990e3abe89adf
ashwani9689@outlook.com <ashwani9689@outlook.com>
2025-03-18 22:12:16 +0530

    PatternProgram3

---
+++ pro2.java
@@ -0,0 +1,14 @@
+public class pro2 {
+    public static void main(String[] args) {
+        System.out.println(" number pattern");
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
+    }
+    
+}
