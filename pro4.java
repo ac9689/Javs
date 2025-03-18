a3010643849390968304b587197c503d8315cc04
ashwani9689@outlook.com <ashwani9689@outlook.com>
2025-03-18 23:47:54 +0530

    patternPro4

---
+++ pro4.java
@@ -0,0 +1,14 @@
+public class pro4{
+    public static void main(String[] args) {
+        System.out.println("Pattern3");
+        int n = 5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=(i+1);j++){
+                System.out.print(" ");
+            
+            }for(int k=1;k<=(2*(n-i)-1);k++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }}
