4b670c130f837ac1169f10d1124ae4e2e0a5e204
ashwani9689@outlook.com <ashwani9689@outlook.com>
2025-04-30 22:59:01 +0530

    DataType and Variable

---
+++ ArryList1.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+public class ArryList1 {
+    public static void main(String[] args){
+        ArrayList <String> List = new ArrayList <String>();
+        List.add("Hello");
+        List.add("World");
+        List.add("Java");
+        System.out.println(List.get(1));
+    }
+}
